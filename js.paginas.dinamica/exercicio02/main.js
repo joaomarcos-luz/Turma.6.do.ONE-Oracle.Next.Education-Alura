42d91a7b729784caff1ba4ca1bf5ebc37e50f460
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-16 11:30:52 -0300

    somando dinamicamente

--- js.paginas.dinamica/exercicio02/main.js
+++ js.paginas.dinamica/exercicio02/main.js
@@ -1,15 +1,25 @@
-const listaTeclas = document.querySelectorAll(".tecla")
-const res = document.querySelector("div")
+const listaTeclas = document.querySelectorAll(".tecla");
+const res = document.querySelector("div");
 
-let contador = 0
-
-const incrementar = () => {
-    contador ++
-    res.innerHTML = `Contador: ${contador}`
-}
-
-let i = 0
+let valorAnterior = 0; // Initialize to store accumulated sum
 
 for (const tecla of listaTeclas) {
-     tecla.addEventListener("click", incrementar)
- }
+  tecla.addEventListener("click", (event) => {
+    const valorAtual = parseInt(event.target.value);
+    valorAnterior += valorAtual; // Update valorAnterior with sum
+    res.innerHTML = `${valorAnterior}`; // Display accumulated value
+  });
+}
+
+// let contador = 0
+
+// const incrementar = () => {
+//     contador ++
+//     res.innerHTML = `Contador: ${contador}`
+// }
+
+// let i = 0
+
+// for (const tecla of listaTeclas) {
+//      tecla.addEventListener("click", incrementar)
+//  }

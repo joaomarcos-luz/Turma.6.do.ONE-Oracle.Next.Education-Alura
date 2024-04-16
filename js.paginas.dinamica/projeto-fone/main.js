d96d1a0705681482be50d81c4059351ef27f1ea5
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-15 21:37:39 -0300

    projeto tel

--- js.paginas.dinamica/projeto-fone/main.js
+++ js.paginas.dinamica/projeto-fone/main.js
@@ -1,5 +1,24 @@
+const listaDeTeclas = document.querySelectorAll("input[type=button]")
+const inpuTel = document.querySelector("input[type=tel]")
 
-const resposta = document.querySelector("input[type=tel]")
 
-const delet = () => document.querySelector("input[type=tel]").value = ""
+for (const teclas of listaDeTeclas) {
+    
+    const tecla = teclas //fosse for normal seria teclas[index]
+
+    tecla.addEventListener("click", () => {
+        inpuTel.value += tecla.value
+    })
+}
+5
+
+
+
+
+
+
+// const inputTel = document.querySelector("input[type=tel]")
+// const delet = () => document.querySelector("input[type=tel]")
+
+// document.querySelector(".delete").onclick = delet
 

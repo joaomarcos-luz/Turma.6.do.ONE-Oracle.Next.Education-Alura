9110ae260cf16fcef76a7420d55581bd1584ad47
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-18 16:07:44 -0300

    pj midia play

--- js.paginas.dinamica/projeto.aluramidi/main.js
+++ js.paginas.dinamica/projeto.aluramidi/main.js
@@ -1,4 +1,11 @@
-const tocarSom = (idElementoSom) => document.querySelector(idElementoSom).play()
+const tocarSom = (seletorAudio) => {
+    const elemento = document.querySelector(seletorAudio);
+    if (elemento === null) {
+        console.log("Elemento não encontrado.");
+    } else {
+        elemento.play(); // Adiciona essa linha para tocar o som
+    }
+}
 
 const listaDeTeclas = document.querySelectorAll(".tecla")
 
@@ -6,12 +13,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumento}`
+    const idAudio = `#som_${instrument}`
 
     tecla.onclick = () => tocarSom(idAudio)
 
-    tecla.onkeydown = () => tecla.classList.add("ativa")
+    tecla.onkeydown = (evento) => {
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add("ativa")
+        }
+    }
 
     tecla.onkeyup = () => tecla.classList.remove("ativa")
-    
+
 }

bd8e79026d0b50643627c3a1c71f4d8130aed778
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-18 10:19:12 -0300

    projeto aluramid

--- js.paginas.dinamica/projeto.aluramidi/main.js
+++ js.paginas.dinamica/projeto.aluramidi/main.js
@@ -6,8 +6,12 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
-
     const idAudio = `#som_${instrumento}`
+
     tecla.onclick = () => tocarSom(idAudio)
+
+    tecla.onkeydown = () => tecla.classList.add("ativa")
+
+    tecla.onkeyup = () => tecla.classList.remove("ativa")
 
 }

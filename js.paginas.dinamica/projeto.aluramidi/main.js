ef4a81e4445fee60de1380a6f8ef7d59190b330e
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-18 16:21:29 -0300

    Update main.js

--- js.paginas.dinamica/projeto.aluramidi/main.js
+++ js.paginas.dinamica/projeto.aluramidi/main.js
@@ -1,9 +1,9 @@
 const tocarSom = (seletorAudio) => {
-    const elemento = document.querySelector(seletorAudio);
+    const elemento = document.querySelector(seletorAudio)
     if (elemento === null) {
-        console.log("Elemento não encontrado.");
+        console.log("Elemento não encontrado.")
     } else {
-        elemento.play(); // Adiciona essa linha para tocar o som
+        elemento.play()
     }
 }
 
@@ -13,7 +13,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrument}`
+    const idAudio = `#som_${instrumento}`
 
     tecla.onclick = () => tocarSom(idAudio)
 

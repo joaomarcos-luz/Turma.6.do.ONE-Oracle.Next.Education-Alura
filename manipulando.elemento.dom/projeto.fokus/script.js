c715bb2576a6552b4d639ab965be3f89d3831260
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-06-29 18:48:56 -0300

    One botao comecar e pausar

--- manipulando.elemento.dom/projeto.fokus/script.js
+++ manipulando.elemento.dom/projeto.fokus/script.js
@@ -1,30 +1,26 @@
 const html = document.querySelector('html')
-const focoBt = document.querySelector(".app__card-button--foco")
+const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
-const musicaFocoInput = document.querySelector('#alternar-musica')
-
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
-const musicaFimContagem = new Audio('/sons/beep.mp3')
-const musicaPauseContagem = new Audio('/sons/pause.mp3')
-const musicaPlayContagem = new Audio('/sons/play.mp3')
-
 const startPauseBt = document.querySelector('#start-pause')
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
 
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5
 let intervaloId = null
 
 musica.loop = true
-musicaFimContagem.loop = true
-musicaPauseContagem.loop = true
-musicaPlayContagem.loop = true
 
 musicaFocoInput.addEventListener('change', () => {
-    if (musica.paused) {
+    if(musica.paused) {
         musica.play()
     } else {
         musica.pause()
@@ -46,31 +42,28 @@ longoBt.addEventListener('click', () => {
     longoBt.classList.add('active')
 })
 
-const alterarContexto = (contexto) => {
-    
-    botoes.forEach( (contexto) => {
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
-
     html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `imagens/${contexto}.png`)
-
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
-        case 'foco':
+        case "foco":
             titulo.innerHTML = `
-                Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
             break;
-        case 'descanso-curto':
+        case "descanso-curto":
             titulo.innerHTML = `
-                Que tal dar uma respirada?<br>
-                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
             break;
-        case 'descanso-longo':
+        case "descanso-longo":
             titulo.innerHTML = `
-                Hora de voltar á superficie,<br>
-                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
-            break;
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
         default:
             break;
     }
@@ -78,31 +71,31 @@ const alterarContexto = (contexto) => {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        zera()
-        alert('Tempo Finalizado!')
+        //32audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
         return
     }
-    
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador' + tempoDecorridoEmSegundos)
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
     if(intervaloId){
-        musicaPlayContagem.pause()
-        musicaPauseContagem.play()
-        zera()
+        audioPausa.play()
+        zerar()
         return
     }
-    musicaPlayContagem.play()
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = 'Pausar'
 }
 
-function zera() {
-    clearInterval(intervaloId)
-    musicaPlayContagem.pause()
-    musicaFimContagem.play()
+function zerar() {
+    clearInterval(intervaloId) 
+    iniciarOuPausarBt.textContent = 'Começar'
     intervaloId = null
 }
+
